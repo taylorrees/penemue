7791297233bc86695ae6d823dd43aebd170ec2ca
Taylor Rees <hello@taylorrees.co>
2016-06-27 11:53:23 +0100

    Added activity to filtering criteria.

--- filter/filter.py
+++ filter/filter.py
@@ -1,10 +1,13 @@
-import nltk
 import json
 import words
-from nltk.tokenize import word_tokenize
+import time
 
 
 def has_keyword(text):
+    """
+    Check for presence of keyword in text.
+    """
+
     for word in words.keywords:
         if word in text.lower():
             return True
@@ -12,19 +15,45 @@ def has_keyword(text):
     return False
 
 
-filtered = []
+def is_active(user, days=10):
+    """
+    Check user activity for specified number of days.
+    """
 
-with open("../json/twitter_profiles.json") as infile:
-    users = json.load(infile)
-    infile.close()
+    # days in seconds
+    seconds = lambda days : days * 24 * 60 * 60
 
-for user in users:
-    if has_keyword(user["description"]) and (not user["protected"]):
-        filtered.append(user)
+    if "status" in user:
+        # get created_at attribute
+        # convert to seconds
+        created_at = user["status"]["created_at"]
+        created_at = time.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")
+        created_at = time.mktime(created_at)
 
-with open("../json/twitter_profiles.json", "w") as outfile:
-    json.dump(filtered, outfile)
-    outfile.close()
+        now = time.time()
+        active_since = now - seconds(days=days)
 
-print("Users: %s" % len(users))
-print("Filtered: %s" % len(filtered))
+        if created_at > active_since:
+            return True
+
+        return False
+
+
+if __name__ == "__main__":
+
+    filtered = []
+
+    with open("../json/twitter_profiles.json") as infile:
+        users = json.load(infile)
+        infile.close()
+
+    for user in users:
+        if has_keyword(user["description"]) and (not user["protected"]) and is_active(user):
+            filtered.append(user)
+
+    with open("../json/twitter_profiles.json", "w") as outfile:
+        json.dump(filtered, outfile)
+        outfile.close()
+
+    print("Users: %s" % len(users))
+    print("Filtered: %s" % len(filtered))
