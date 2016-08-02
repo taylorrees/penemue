a1172668df97999ee79f7aa3bb75544942af4e5b
Taylor Rees <hello@taylorrees.co>
2016-08-02 23:20:21 +0100

    Updates to util imports

--- penemue/example_credentials.py
+++
@@ -1,7 +0,0 @@
-# Remove 'example_' from the file name
-# to use this file.
-
-app_key = 'YOUR_TWITTER_APP_KEY'
-app_secret = 'YOUR_TWITTER_APP_SECRET'
-auth_token = 'YOUR_TWITTER_AUTH_TOKEN'
-auth_secret = 'YOUR_TWITTER_AUTH_SECRET'
--- penemue/utils/__init__.py
+++ penemue/utils/__init__.py
@@ -2,3 +2,4 @@ from .monitor_tweets import MonitorTweets
 from .monitor_users import MonitorUsers
 from .collect import Collect
 from .config import db
+from .filter import *
